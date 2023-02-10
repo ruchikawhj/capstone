339348e9d74a35e44f5951a09f390485e58655c0
ruchikawhj <74497291+ruchikawhj@users.noreply.github.com>
2023-02-10 15:21:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var bgImg, bg;
 var pipeupImg, pipe1;
 var pipedownImg, pipe2;
 var birdImg, bird;
-var score;
+var score=0;
 var gameState=PLAY;
 var restartImg, restart;
 
@@ -12,28 +12,32 @@ function preload(){
  birdImg=loadImage("bird.png");
  bgImg=loadImage("bg.png");
  pipeupImg=loadImage("pipeUp.png");
- pipedownImg=loadImage("pipeDown.png")
- restart=loadImage("restart.png");
+ pipedownImg=loadImage("pipedown.png")
+ restartImg=loadImage("restart.png");
  
 }
 
 function setup() {
 
 createCanvas(600,600);
-bg=createSprite(300,300);
+bg=createSprite(300,300,600,600);
 bg.addImage(bgImg);
+bg.scale=2.5
+
 
 bird=createSprite(20,256);
-bird.scale=0.5;
+
 bird.addImage(birdImg);
+bird.debug=true;
 
-pipe1group=new Group();
-pipe2group=new Group();
 
-restart.createSprite(300,300);
-restart.addImage(restart);
-restart.scale=0.5
-restart.visible=false
+
+restart=createSprite(300,250);
+restart.addImage(restartImg );
+restart.scale=0.2
+restart.visible=false;
+pipe1Group=new Group();
+pipe2Group=new Group();
 }
 
 function draw() {
@@ -41,45 +45,49 @@ if(gameState===PLAY){
     pipe_move();
 }
 if(keyDown("space")){
-    bird.y=bird.y -20
+    bird.velocityY=-8;
 }
-if(bird.isTouching(pipe1group)|| bird.isTouching(pipe2group)){
+bird.velocityY+=0.8
+if(pipe1Group.isTouching(bird)|| pipe2Group.isTouching(bird)||bird.y>=600){
     gameState=END;
 }
 if(frameCount%100===0){
     score++;
 }
 if(gameState===END){
+    bird.y=300
     bird.visible=false;
     restart.visible=true;
-    pipe1.setVelocityXEach(0);
-    pipe2.setVelocityXEach(0);
-    pipe1.setLifeTime(-1);
-    pipe1.setLifeTime(-1);
+    pipe1Group.setVelocityXEach(0);
+    pipe2Group.setVelocityXEach(0);
+    pipe1Group.setLifetimeEach(-1);
+    pipe2Group.setLifetimeEach(-1);
 }
 if(mousePressedOver(restart)){
     reset();
 
 }
 drawSprites();
-textSize(50);
-text("score"+score,575,500);
+textSize(30);
+text("score: "+score,450,50);
 
 }
 
 function pipe_move(){
 if(frameCount%50===0){
-    pipe1 = createSprite(150,0);
+    pipe1 = createSprite(600,0);
     pipe1.addImage(pipeupImg);
     pipe1.y = random(0,50);
+    pipe1.debug=true;
     pipe1.velocityX = -2;
     pipe1Group.add(pipe1);
     pipe1Group.setLifetimeEach(144);
     
-    pipe2 = createSprite(200,600);
+    pipe2 = createSprite(600,600);
     pipe2.addImage(pipedownImg);
     pipe2.y = random(462,512);
     pipe2.velocityX = -2;
+    pipe2.debug=true;
     pipe2Group.add(pipe2);
     pipe2Group.setLifetimeEach(100);
  }
